fafd6ffb9863ea4cd512ba3fab8e5a5e918c9878
lido22 <ahmedwa1999@gmail.com>
2021-06-04 01:07:08 +0200

    fixed database

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ const fs = require('fs');
 let rawdata = fs.readFileSync('tracks.json');
 let tracks = JSON.parse(rawdata);
 let tracksUpdated = []
-let day = 1
-let month = 1
-let year = 2017
+let offset = 1
 counter = 1
 var d = new Date("1/1/2017");
 
 tracks.map((track) => {
     track["Date"] = d.getDay() + 1 + "/" + d.getMonth() + 1 + "/" + d.getFullYear()
     if (!(counter % 200)) {
-        d.setDate(d.getDate() + 1);
+        d.setDate(d.getDate() + offset);
+        offset *= -1 
     }
     counter++
     tracksUpdated.push(track)
 })
-fs.writeFileSync("trackUpdate.json", JSON.stringify(tracksUpdated));
+fs.writeFileSync("trackUpdate.json", JSON.stringify(tracksUpdated , null, 4));
